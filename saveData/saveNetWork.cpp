87a237275177f5d105d65443f7e6cb436e36056f
TanDongXu <sa614149@mail.ustc.edu.cn>
2016-03-16 10:28:43 +0800

    add comment

--- saveData/saveNetWork.cpp
+++ saveData/saveNetWork.cpp
@@ -1,6 +1,6 @@
 #include"saveNetWork.h"
 
-
+/*save network parameter*/
 void saveNetWork()
 {
 	char fileName[50];
@@ -8,7 +8,6 @@ void saveNetWork()
 	int imageSize = config::instanceObjtce()->get_imageSize();
 	int normalized_width = config::instanceObjtce()->get_normalizedWidth();
 
-	//注意：工程文件所在的目录是当前目录，相对路径应该以该目录为参照物，而与可执行文件的存放目录无关
 	sprintf(fileName,"models/net_normalized_%d_%d.txt", imageSize, normalized_width);
 	FILE *file = fopen(fileName, "w");
 

dc6fdd458beed50ef1e30baef71b038cc836ed2a
TanDongXu <sa614149@mail.ustc.edu.cn>
2017-03-28 16:41:15 +0800

    modify main

--- main.cpp
+++ main.cpp
@@ -1,32 +1,35 @@
 /*
 * main.cpp
-*
 *  Created on: Nov 19, 2015
 *      Author: tdx
 */
 #include<iostream>
 #include <cuda.h>
-#include"./examples/mnist.h"
-#include"./examples/cifar-10.h"
+#include"examples/mnist.h"
+#include"examples/cifar-10.h"
+#include"examples/dynamic_g_model/dynamic_g_entry.hpp"
+#include<glog/logging.h>
 
-/*use cuDNN Acceleration*/
-
-int main(void)
+int main(int argc, char** argv)
 {
-    cout<<"Select the dataSet to run:"<<endl<<"1.MNIST    2.CIFAR-10"<<endl;
+    FLAGS_alsologtostderr = 1;
+    google::InitGoogleLogging(argv[0]);
+
+    LOG(INFO) << "Select the DataSet to Run:";
+    LOG(INFO) << "1.MNSIT     2.CIFAR-10    3.Dynamic Generation Model";
 
     cudaSetDevice(0);
     int cmd;
     cin>> cmd;
-
     if(1 == cmd)
-    runMnist();
+        runMnist();
     else if(2 == cmd)
-    runCifar10();
-    else{
-        cout<<"DataSet select Error!"<<endl;
-        exit(0);
-    }
+         runCifar10();
+    else if(3 == cmd)
+        dynamic_g_entry();
+    else
+        LOG(FATAL) << "DataSet Select Error.";
+
     return 0;
 }
 

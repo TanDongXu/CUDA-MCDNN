9c6323b121abd3d5595e7f2dcd0eadd969afdb79
TanDongXu <sa614149@mail.ustc.edu.cn>
2017-03-28 16:36:59 +0800

    add node dynamic generate

--- composite/NodeFission.cpp
+++ composite/NodeFission.cpp
@@ -38,6 +38,8 @@ void NodeFission(LayersBase* splitNode, LayersBase* nextNode) {
         }
     }
 
+    //这还要修改next的输入名字,因为只是删除next prev的指针,不用深度优先会出现错误
+    nextConfig->_input = configFiss->_name;
 
     //modify Layers
     LayersBase* layerFiss = FissionFactory::instanceObject()->createLayer(splitNode);
@@ -57,16 +59,127 @@ void NodeFission(LayersBase* splitNode, LayersBase* nextNode) {
             break;
         }
     }
-//delete prev
-for (l_iter = nextNode->prevLayer.begin(); l_iter != nextNode->prevLayer.end(); l_iter++) {
-    if ((*l_iter)->_name == (splitNode->_name)) {
-        nextNode->prevLayer.erase(l_iter);
-        break;
+    //delete prev
+    for (l_iter = nextNode->prevLayer.begin(); l_iter != nextNode->prevLayer.end(); l_iter++) 
+    {
+        if ((*l_iter)->_name == (splitNode->_name)) {
+            nextNode->prevLayer.erase(l_iter);
+            break;
+        }
     }
-}
-//insert one node
-nextNode->insertPrevLayer(layerFiss);
+    //修改名字
+    nextNode->_inputName = layerFiss->_name;
+    //insert one node
+    nextNode->insertPrevLayer(layerFiss);
 
+}
+
+//node fission for dynamic_generate model
+void nodeGenerate(config* endConfig) 
+{
+    vector<LayersBase*>::iterator l_iter;
+    vector<configBase*>::iterator c_iter;
+
+    configBase* templateConfig = endConfig->getFirstLayers();
+    configBase* curConfig = config::instanceObjtce()->getFirstLayers();
+    configBase* newConfig_prev = NULL;
+    configBase* newConfig_next = NULL;
+    //find the first different node 
+    while(0 != templateConfig->_next.size() && 0 != curConfig->_next.size())
+    {
+        if(templateConfig->_type == "SOFTMAX" || curConfig->_type == "SOFTMAX") 
+            LOG(FATAL) << " Config Table Error, SoftMax not finally layer.";
+        if(templateConfig->_name == curConfig->_name)
+        {
+            newConfig_prev = curConfig;
+            templateConfig = templateConfig->_next[0];
+            curConfig = curConfig->_next[0];
+        }else
+        {
+            //this time, templateConfig point to the first different node
+            newConfig_next = curConfig;
+            break;
+        }
+    }
+
+    cout<<"enter-1"<<endl;
+    //make sure the point is not null 
+    if(NULL == newConfig_prev && NULL == newConfig_next) return;
+    //modify config
+    configBase* newConfig = DynamicFactory::instanceObject()->createConfig(templateConfig);
+    //necessary
+    newConfig->_prev.clear();
+    newConfig->_next.clear();
+
+    cout<<newConfig->_name<<" "<<newConfig->_type<<" "<<newConfig->_input<<" "<<newConfig->_prev.size()<<" "<< newConfig->_next.size()<<endl;
+
+    newConfig_prev->_next.push_back(newConfig);
+    newConfig->_prev.push_back(newConfig_prev);
+    newConfig->_next.push_back(newConfig_next);
+    newConfig_next->_prev.push_back(newConfig);
+
+    cout<<newConfig->_name<<" "<<newConfig->_type<<" "<<newConfig->_input<<" "<<newConfig->_prev[0]->_name<<" "<< newConfig->_next[0]->_name<<endl;
+    //insert into origin config table
+    cout<<"no hidden-1"<<endl;
+    config::instanceObjtce()->insertLayerByName(newConfig->_name, newConfig);
+    cout<<"no hidden"<<endl;
+    // prev layer delete origin next layer point
+    for (c_iter = newConfig_prev->_next.begin(); c_iter != newConfig_prev->_next.end(); c_iter++) 
+    {
+        if ((*c_iter)->_name == (newConfig_next->_name)) {
+            newConfig_prev->_next.erase(c_iter);
+            break;
+        }
+    }
+    // next layer delete origin prev layer point
+    for (c_iter = newConfig_next->_prev.begin(); c_iter != newConfig_next->_prev.end(); c_iter++) {
+        if ((*c_iter)->_name == (newConfig_prev->_name)) {
+            newConfig_next->_prev.erase(c_iter);
+            break;
+        }
+    }
+    
+    cout<<newConfig_next->_input<<endl;
+    //modify nextConfig input name
+    newConfig_next->_input = newConfig->_name;
+    cout<<newConfig_next->_input<<endl;
+
+
+    //modify Layers by new config node
+    LayersBase* newLayer = DynamicFactory::instanceObject()->createLayer(newConfig);
+    newLayer->nextLayer.clear();
+    newLayer->prevLayer.clear();
+    //prev layer
+    LayersBase* newLayer_prev = Layers::instanceObject()->getLayer(newConfig->_input);
+    //next layer
+    LayersBase* newLayer_next = Layers::instanceObject()->getLayer(newConfig_next->_name);
+    // new node insert next layer
+    newLayer->insertNextlayer(newLayer_next);
+    //insert into layer table
+    Layers::instanceObject()->storLayers(newLayer_prev->_name, newLayer->_name, newLayer);
+    //delete next
+    for (l_iter = newLayer_prev->nextLayer.begin(); l_iter != newLayer_prev->nextLayer.end(); l_iter++) 
+    {
+        if ((*l_iter)->_name == newLayer_next->_name) {
+            newLayer_prev->nextLayer.erase(l_iter);
+            break;
+        }
+    }
+    //delete prev
+    for (l_iter = newLayer_next->prevLayer.begin(); l_iter != newLayer_next->prevLayer.end(); l_iter++) 
+    {
+        if ((*l_iter)->_name == (newLayer_prev->_name)) 
+        {
+            newLayer_next->prevLayer.erase(l_iter);
+            break;
+        }
+    }
+
+    //must: modify nextlayer input name
+    newLayer_next->_inputName = newLayer->_name;
+    //insert one node
+    newLayer_next->insertPrevLayer(newLayer);
+    cout<<"create layer success"<<endl;
 }
 
 /*softMaxLayer fission*/
--- composite/NodeFission.h
+++ composite/NodeFission.h
@@ -8,6 +8,7 @@
 #ifndef NODEFISSION_H_
 #define NODEFISSION_H_
 
+#include<glog/logging.h>
 #include"../layers/LayersBase.h"
 #include"../common/utility.cuh"
 #include"../config/config.h"
@@ -24,7 +25,7 @@
 
 void NodeFission(LayersBase* splitNode, LayersBase* nextNode);
 void softmaxFission(LayersBase* splitNode);
-
+void nodeGenerate(config* endConfig);
 
 //static factory
 class FissionFactory
@@ -153,6 +154,113 @@ class FissionFactory
             exit(0);
         }
 }
+
+};
+
+//static factory
+class DynamicFactory
+{
+    public:
+    static DynamicFactory* instanceObject(){
+
+        static DynamicFactory* factory = new DynamicFactory();
+        return factory;
+    }
+
+    static LayersBase* createLayer(configBase* node)
+    {
+
+        string nodeType = node->_type;
+
+        if(string("DATA") == nodeType)
+        {
+            return new DataLayer(node);
+        }else if(string("CONV") == nodeType)
+        {
+            return new ConvLayer(node);
+        }else if(string("POOLING") == nodeType)
+        {
+            return new PoolLayer(node);;
+
+        }else if(string("HIDDEN") == nodeType)
+        {
+            return new HiddenLayer(node);
+        }else if(string("SOFTMAX") == nodeType)
+        {
+            return new SoftMaxLayer(node);
+        }else if(string("ACTIVATION") == nodeType)
+        {
+            return new ActivationLayer(node);
+        }else if(string("LRN") == nodeType)
+        {
+            return new LRNLayer(node);
+        }else if(string("DROPOUT") == nodeType)
+        {
+            return new DropOutLayer(node);
+        }else
+        {
+            LOG(FATAL) << "NodeGenerate: Layer Type " << nodeType <<" not exist";
+            return NULL;
+        }
+    }
+
+    static configBase* createConfig(configBase* templateConfig)
+    {
+        string nodeType = templateConfig->_type;
+
+        if (string("DATA") == nodeType)
+        {
+            configData* tmp = (configData*) templateConfig;
+            configData* config = new configData( *tmp );
+            return config;
+
+        } else if (string("CONV") == nodeType)
+        {
+            configConv* tmp  = (configConv*)templateConfig;
+            configConv* config = new configConv(*tmp);
+            return config;
+
+        } else if (string("POOLING") == nodeType)
+        {
+            configPooling* tmp = (configPooling*)templateConfig;
+            configPooling* config = new configPooling(*tmp);
+            return config;
+
+        } else if (string("HIDDEN") == nodeType)
+        {
+            configHidden* tmp = (configHidden*)templateConfig;
+            configHidden* config =new configHidden (*tmp);
+            return config;
+
+        } else if (string("SOFTMAX") == nodeType)
+        {
+            configSoftMax* tmp = (configSoftMax*)templateConfig;
+            configSoftMax* config = new configSoftMax(*tmp);
+            return config;
+
+        } else if (string("ACTIVATION") == nodeType)
+        {
+            configActivation* tmp = (configActivation*)templateConfig;
+            configActivation* config = new configActivation(*tmp);
+            return config;
+
+        } else if (string("LRN") == nodeType)
+        {
+            configLRN* tmp = (configLRN*)templateConfig;
+            configLRN* config = new configLRN(*tmp);
+            return config;
+
+        } else if (string("DROPOUT") == nodeType)
+        {
+            configDropOut* tmp = (configDropOut*)templateConfig;
+            configDropOut* config = new configDropOut(*tmp);
+            return config;
+        }else
+        {
+            LOG(FATAL) << "NodeGenerate: Config type " << nodeType <<" not exist";
+            return NULL;
+        }
+    }
 
 };
 
